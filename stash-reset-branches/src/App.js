83e5d53791355160be4702c5fb1e707f072c8390
saihari08 <bsaihari08@gmail.com>
2024-08-29 14:45:58 +0530

    Add files via upload

---
+++ stash-reset-branches/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Details of RRR</h1>
+
+      <h2>Rajamouli</h2>
+      <img src="./Rajamouli/Rajamouli.jpg"></img>
+
+    
+      <h2>Ram Charan</h2>
+    <img src="RamCharan/RamCharan.jpg"></img>
+
+    </div>
+  );
+}
+
+export default App;
